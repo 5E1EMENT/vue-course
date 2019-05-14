1779b47ac127d8f0ec31f2a84be117e88625b443
Nikitas Daniil <daniil_nikitas@mail.ru>
2019-05-14 21:26:30 +0600

    vue router create nav + active link

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="container pt-2">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="collapse navbar-collapse">
+        <ul class="navbar-nav">
+          <!--<li class="nav-item active">-->
+            <!--<router-link class="nav-link" to="/">Home</router-link>-->
+          <!--</li>-->
+          <router-link tag="li" class="nav-item" exact to="/" active-class="active">
+            <a class="nav-link">Home</a>
+          </router-link>
+          <router-link tag="li" class="nav-item" to="/cars" active-class="active">
+            <a class="nav-link">Cars</a>
+          </router-link>
+        </ul>
+      </div>
+    </nav>
     <router-view></router-view>
   </div>
 </template>
