54c2b91cacc7c74bfd30cb49fbe8cbf16544a9dc
Nikitas Daniil <daniil_nikitas@mail.ru>
2019-05-14 20:23:39 +0600

    password validate

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -14,23 +14,66 @@
         <div class="invalid-feedback" v-if="!$v.email.required">Email field is required</div>
         <div class="invalid-feedback" v-if="!$v.email.email">This field should be an email</div>
       </div>
+      <div class="form-group">
+        <label for="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          class="form-control"
+          :class="{'is-invalid': $v.password.$error}"
+          v-model="password"
+          @blur="$v.password.$touch()"
+        >
+        <div class="invalid-feedback" v-if="!$v.password.minLength">
+          Min length of password is {{ $v.password.$params.minLength.min }}. Now it is {{ password.length }}
+        </div>
+
+      </div>
+      <div class="form-group">
+        <label for="confirm">Confirm Password</label>
+        <input
+          type="password"
+          id="confirm"
+          class="form-control"
+          :class="{'is-invalid': $v.confirmPassword.$error}"
+          v-model="confirmPassword"
+          @blur="$v.confirmPassword.$touch()"
+        >
+        <div class="invalid-feedback" v-if="!$v.confirmPassword.sameAs">
+          Password is not the same
+        </div>
+
+      </div>
     </form>
   </div>
 </template>
 
 <script>
-  import {required, email} from 'vuelidate/lib/validators'
+  import {required, email, minLength, sameAs} from 'vuelidate/lib/validators'
   export default {
     data() {
       return {
-        email: ''
+        email: '',
+        password: '',
+        confirmPassword: ''
       }
     },
     validations: {
       email: {
         required,
         email
+      },
+      password: {
+        minLength: minLength(6)
+      },
+      confirmPassword: {
+        sameAs: sameAs('password')
       }
+      // confirmPassword: {
+      //   sameAs: sameAs((vue) => {
+      //     return vue.password
+      //   })
+      // }
     }
   }
 </script>
