5789fb5c0d015114a3f86e27f969805177a5a65e
Nikitas Daniil <daniil_nikitas@mail.ru>
2019-05-15 21:20:09 +0600

    POST REQUEST

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="container pt-2">
+    <div class="form-group">
+      <label for="name">Name</label>
+      <input type="text" class="form-control" id="name" v-model.trim="carName">
+    </div>
+    <div class="form-group">
+      <label for="year">Car Year</label>
+      <input type="text" class="form-control" id="year" v-model.number="carYear">
+    </div>
 
+    <button class="btn btn-success" @click="createCar">Create car</button>
   </div>
 </template>
 
@@ -8,7 +17,24 @@
   export default {
     data () {
       return {
-        title: 'TEEEXT'
+        carName: '',
+        carYear: 2019
+      }
+    },
+    methods: {
+      createCar () {
+        const car = {
+          name: this.carName,
+          year: this.carYear
+        }
+        this.$http.post('http://localhost:3000/cars', car)
+          .then(response => {
+            console.log(response)
+            return response.json()
+          })
+          .then(newCar => {
+            console.log(newCar)
+          })
       }
     },
     computed: {
